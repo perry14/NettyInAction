392abb0a098d113f9d637832fb586e64f1f4fb0d
perry14 <yuezuofang@vip.qq.com>
2019-01-02 15:53:07 +0800

    new Time Clint/Server using En/Decoder.

---
+++ src/main/java/netty4x/user/guide/time2/TimeClientHandler.java
@@ -0,0 +1,20 @@
+package netty4x.user.guide.time2;
+
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+
+public class TimeClientHandler extends ChannelInboundHandlerAdapter {
+
+	@Override
+	public void channelRead(ChannelHandlerContext ctx, Object msg) {
+		UnixTime m = (UnixTime) msg;
+		System.out.println(m);
+		ctx.close();
+	}
+
+	@Override
+	public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
+		cause.printStackTrace();
+		ctx.close();
+	}
+}
